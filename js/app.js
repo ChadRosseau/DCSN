35294e7598cd1bffae9412e319227819cf6eb405
Chad Rosseau <dahc.creeper@icloud.com>
2020-12-13 23:45:02 +0800

    added navbar

--- js/app.js
+++ js/app.js
@@ -1,12 +1,67 @@
 const express = require("express");
-const app = express();
+const expressApp = express();
 const port = 3000;
 
+angular.module("myApp", [])
 
-app.get('/', (req, res) => {
+var app = angular.module("myApp", ["ngRoute", "ui.router", "firebase"]);
+
+
+expressApp.get('/', (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, function() {
+expressApp.listen(port, function() {
     console.log(`Server listening on port ${port}`);
 })
+
+
+app.directive('navbar', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/navbar.html'
+    }
+});
+
+app.directive('myfooter', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/footer.html'
+    }
+});
+
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/home", {
+            templateUrl: "home.html",
+        })
+
+    .when("/about", {
+        templateUrl: "about.html",
+    })
+
+    .when("/contact", {
+        templateUrl: "contact.html",
+    })
+
+    .when("/profile", {
+        templateUrl: "profile.html",
+    })
+
+    .when("/tickets", {
+        templateUrl: "tickets.html",
+    })
+
+    .when("/login", {
+        templateUrl: "login.html",
+    })
+
+    .when("/secret", {
+        templateUrl: "secret.html",
+    })
+
+    .otherwise({
+        redirectTo: '/home'
+    });
+});
--- js/start.js
+++ js/start.js
@@ -1,2 +1,4 @@
 // Initialize Firebase Real-Time Database
 var database = firebase.database();
+
+console.log("connected")
