699d1af962268adfb66667f0fc87cf5397d15a7a
Chad Rosseau <dahc.creeper@icloud.com>
2020-12-16 17:37:32 +0800

    installed firebase

--- newApp/src/app/app.module.ts
+++ newApp/src/app/app.module.ts
@@ -1,9 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Firebase imports
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// Component imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1qHH3zsiDBGVJL1lusKecWhZjTDyo1AU",
+  authDomain: "dcsn-e8f7a.firebaseapp.com",
+  databaseURL: "https://dcsn-e8f7a-default-rtdb.firebaseio.com",
+  projectId: "dcsn-e8f7a",
+  storageBucket: "dcsn-e8f7a.appspot.com",
+  messagingSenderId: "327191415154",
+  appId: "1:327191415154:web:a4653bfb4629a94f79f5eb",
+  measurementId: "G-300TVBWZ72"
+};
 
 @NgModule({
   declarations: [
@@ -11,6 +29,10 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
     BrowserModule,
     AppRoutingModule
   ],
