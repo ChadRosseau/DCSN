aecf70520f31ccfc61b1eb0a29c3a48c03ba6350
Chad Rosseau <dahc.creeper@icloud.com>
2021-02-23 23:17:47 +0800

    broken version of editor, trying to do images

--- newApp/src/app/components/create-post/create-post.component.ts
+++ newApp/src/app/components/create-post/create-post.component.ts
@@ -6,6 +6,11 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AsyncSubject, Subject } from 'rxjs';
 import { maxLength } from './maxlength.validator';
 import { TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
+import { express } from 'express';
+import { jwt } from 'jsonwebtoken';
+import { cors } from 'cors';
+
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-create-post',
@@ -15,12 +20,49 @@ import { TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
 export class CreatePostComponent implements OnInit {
 
   currentSubcategories;
-
+  privateKey;
   tinyInit;
 
-  constructor(public auth: AuthService, public sharedData: SharedDataService) { }
+  // public express = express;
+  // public jwt = jwt;
+  // public cord = cors;
+
+  constructor(
+    public auth: AuthService,
+    public sharedData: SharedDataService,
+    // public express: express,
+    // public jwt: jwt,
+    // public cord: cors
+  ) { }
+
 
   ngOnInit(): void {
+
+    const app = express();
+    app.use(cors());
+    this.privateKey = environment.privateKey;
+
+    app.post("/jwt", function (req, res) {
+      const payload = {
+        sub: this.auth.userKey, // Unique user id string
+        name: this.auth.userKey, // Full name of user
+        exp: Math.floor(Date.now() / 1000) + 60 * 10, // 10 minutes expiration
+      };
+      try {
+        const token = jwt.sign(payload, this.privateKey, { algorithm: "RS256" });
+        res.set("content-type", "application/json");
+        res.status(200);
+        res.send(
+          JSON.stringify({
+            token: token,
+          })
+        );
+      } catch (e) {
+        res.status(500);
+        res.send(e.message);
+      }
+    });
+
     this.currentSubcategories = [];
     console.log(TINYMCE_SCRIPT_SRC);
     this.tinyInit = {
@@ -31,8 +73,17 @@ export class CreatePostComponent implements OnInit {
         "advlist autolink lists link image charmap print preview hr anchor pagebreak",
         "searchreplace wordcount visualblocks visualchars code fullscreen",
         "insertdatetime media nonbreaking save table contextmenu directionality",
-        "emoticons template paste textcolor colorpicker textpattern"
+        "emoticons template paste textcolor colorpicker textpattern",
+        "tinydrive"
       ],
+      tinydrive_token_provider: function (success, failure) {
+        success({ token: "jwt-token" }); failure('Could not create a jwt token');
+      },
+      // tinydrive_upload_path: "/some/other/path",
+      toolbar: "insertfile",
+      menu: {
+        insert: { title: "Insert", items: "insertfile" },
+      },
       menubar: false,
       min_height: 150
     }
@@ -59,6 +110,8 @@ export class CreatePostComponent implements OnInit {
     this.editorSubject.next(e.editor);
     this.editorSubject.complete();
   }
+
+
 
   // createArticle(category, subcategory) {
   //   // Set category and subcategory info
--- newApp/src/environments/environment.ts
+++ newApp/src/environments/environment.ts
@@ -13,7 +13,32 @@ export const environment = {
     messagingSenderId: "481286937282",
     appId: "1:481286937282:web:55d80fac02f2ce18dbf95b",
     measurementId: "G-EZ9TK2EQ7R"
-  }
+  },
+  privateKey: `MIIEowIBAAKCAQEAnJ46yTu97g0EJ4Lex9isOBQpqWWgVaI+HuD5axXH3huvx/FL
+               HCYchdIvEGFn/zKY8tKvMY0GYI1yBwfmJqm1mB5ivAY/+eRqVBI8gmgxRGzJKLZB
+               rHwjCikcj/7L08yJeFviWBaVIiRgu2NlmhkbXc77UzAxeYzCzCtXdLAjUolGUIXG
+               ATvGfrEL65t68rJLN5K4QtpJM/fZR8rQ1tq54oATG33spM5H+qPyo0cFBbbAy6i4
+               JirURAL8ZVeXjbFO6ZwFE/0vs1PQ9bb8jBAtRLydlLOidb8V+r38ZxPqN563ME2c
+               ftEnCC73GllA02/AT9lh6+u9ur3lv4+SMXzbCwIDAQABAoIBADnzqpjvjDXbhx//
+               ouOb3w3ZGBlgS1WkdQwD/waKxqv+3qpDNKGz0HYL8fJDuLGLTWD8aGsTXbq4W+sS
+               Fy1TOqiRBcPBJEcZJ+b34H/4gdJp1n1EA0UJogWOdM0rYMx8MJnilfw2TvSVNV3h
+               R6UfiSrK/eAoQwZQ+XuGxhOdYVSWdnU0tt3/VHPp7zWHgixG6r4Jh4Hzxe49YWJr
+               b9tOppn1waufepDTimaxX11w22d7/JVZol9xWwrpNWYw7XJxVljGAR8u+fb6LyA7
+               0Osu9oMkoapLCIu1qo3g1Pp5vq9MIYTNVAsWAJsAsgF4T8FF7/5G/sdYaCEncvQZ
+               QBnaECECgYEA1f2mJmlUmYiicnCr2MFIemN0Tni0U7UMVY2UXUryCwqLyBIcA5lQ
+               Q4i0ljhSjnblhqnCjNxRRFLA1U9pqa5huLo+o2HX5jkh23KaafNk0IR5RSO6vMgo
+               H3l9VRphEUWxfDD+5uOW3OCCsVe4ZUb4DiPsAoIAr4FmHF+pR3lMs2kCgYEAu10/
+               uxi8JEqiG2WKZd/R1KlBd1779zSxSarq/Tgavms3aqD8VO5gMTlf5ROFoURFtYFJ
+               grUsEX3Ow8DhlOg2Y9l4xE3VuDe1HUkPmMsMFgUfdxv7wn7M125HMItFZ4iwWhZl
+               hikA8uzGIDKhsHCERCW7BvdSv8AHni5twNvDMFMCgYBxzugZz7Utvsxevuu2aP2K
+               EfDedzW4uzZnHdhFUcGu+5nh+zeo9lYIxOD+b9AAct2ZpBmTXu1dwAFMsWKJ2vOg
+               EnWT4Uq457PJCYd0R/1bJMl2aw06r90e+DcfFtt/GyjM2yYGWA55RHJdrUClpHvy
+               0UoI/2kr7k8HUh0upSWU2QKBgQCSSw3u5TJJ1+hZIeB9CNSKipluPkDYVVE0JvsS
+               OhNPqOF9JTPAkNuUs6q3b5XARFF1lfwyzYkaIzQlTl4UaXfGYNBV3jxIeV61/gzq
+               jWfjbZ6S3sulomIIt7doJOZNH8IqpSAiL7Po+Yz9KOtsm336w8cTelsnEyYfkTrr
+               AeyWtwKBgCmGqb1PPSfsfszO9jmEHP00SJMojzrSwNLnyun6UJaFmr0/N6dXvXqv
+               zovjU5unC76z+1TkthlNOO0rXXrqJ/+kahb1gpbc2s3wszW1tnMJCimcXnKbJbwq
+               Ug6u5iN0QwT2QM9HOtUsVdzeIlqfZUJUr2dbMnnvQO99ACqe280u`
 };
 
 /*
