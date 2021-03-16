8a31c8167fa077324b448d29ea90f9c2fa491bd1
Chad Rosseau <dahc.creeper@icloud.com>
2021-03-16 22:55:06 +0800

    finished about us besides hero 4

--- newApp/src/app/components/about/about.component.ts
+++ newApp/src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedDataService } from 'src/app/services/shared-data.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  images;
 
-  constructor() { }
+  constructor(public sharedData: SharedDataService) { }
 
   ngOnInit(): void {
+    this.images = {
+      hero1Background: this.sharedData.dcImages.groupPhoto,
+      hero3Background: this.sharedData.dcImages.map,
+      hero5Background: this.sharedData.dcImages.groupwork,
+    };
   }
 
 }
