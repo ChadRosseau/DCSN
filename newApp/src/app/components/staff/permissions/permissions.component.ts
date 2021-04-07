14d727f42526b6de13c63ef51f8fe232c1579b56
Chad Rosseau <dahc.creeper@icloud.com>
2021-04-07 11:14:13 +0800

    some changes to permissions page

--- newApp/src/app/components/staff/permissions/permissions.component.ts
+++ newApp/src/app/components/staff/permissions/permissions.component.ts
@@ -12,6 +12,8 @@ import { SharedDataService } from '@services/shared-data.service';
 })
 export class PermissionsComponent implements OnInit {
   staff;
+  users;
+
   constructor(public auth: AuthService, public sharedData: SharedDataService) { }
 
   ngOnInit(): void {
@@ -23,20 +25,30 @@ export class PermissionsComponent implements OnInit {
           permission: data[key]
         };
       })
-      // console.log(this.staff);
-      // for (let i = 0; i < this.staff.length; i++) {
-      //   this.staff[i] = {
-      //     id: this.staff[i][0],
-      //     permission: this.staff[i][1]
-      //   }
-      // }
-      console.log(this.staff);
     }).then(() => {
       this.auth.db.database.ref('profiles').once('value', (snapshot) => {
         let data = snapshot.val();
+        this.users = data;
         this.staff.forEach(member => {
           member['firstName'] = data[member.id]['firstName'];
-          member['lastName'] = data[member.id]['lastName']
+          member['lastName'] = data[member.id]['lastName'];
+          let rolesArray = data[member.id]['roles'];
+          let rolesHTML = "";
+          rolesArray.forEach(role => {
+            if (role == "contributor") {
+              rolesHTML += `<i class="roleIcon far fa-edit"></i>`;
+            } else if (role == "moderator") {
+              rolesHTML += `<i class="roleIcon far fa-copy"></i>`;
+            } else if (role == "technology") {
+              rolesHTML += `<i class="roleIcon fas fa-laptop-code"></i>`;
+            } else if (role == "graphics") {
+              rolesHTML += `<i class="roleIcon fas fa-palette"></i>`;
+            } else {
+              console.log("unknown role found");
+            }
+          });
+          console.log(rolesHTML)
+          member['rolesHTML'] = rolesHTML;
         });
         this.staff.sort((a, b) => {
           if (a['permission'] < b['permission']) {
