7c40628d571983a81697108152916f38786285ea
Chad Rosseau <dahc.creeper@icloud.com>
2021-06-22 21:03:26 +0800

    FINAL bugfix async

--- newApp/src/app/components/article/article.component.ts
+++ newApp/src/app/components/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, NgZone } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SharedDataService } from '@services/shared-data.service';
 import { AuthService } from '@services/auth.service';
@@ -12,17 +12,22 @@ import { AuthService } from '@services/auth.service';
 export class ArticleComponent implements OnInit {
   currentArticleId: string;
   article;
+  author;
   dataLoaded;
 
-  constructor(public auth: AuthService, private route: ActivatedRoute, public sharedData: SharedDataService, private cd: ChangeDetectorRef) { }
+  constructor(public auth: AuthService, private route: ActivatedRoute, public sharedData: SharedDataService, private cd: ChangeDetectorRef, private ngZone: NgZone) {
+    this.author = {
+      firstName: "hi",
+      lastName: "hello",
+      photoURL: "What?"
+    };
+  }
 
   ngOnInit(): void {
 
     // Set pagedata defaults
+    this.article = {};
     this.dataLoaded = false;
-    this.article = {
-      author: {}
-    };
 
     // Fetch current article id from URL.
     this.currentArticleId = this.route.snapshot.paramMap.get('articleId');
@@ -31,31 +36,26 @@ export class ArticleComponent implements OnInit {
     const dbPortfolioRef = this.auth.db.database.ref(`articles/moderating/${this.currentArticleId}`);
     dbPortfolioRef.once('value', (snapshot) => {
       let articleData = snapshot.val()
-
+      this.article = articleData;
+    }).then(() => {
       // Code to get author data
-      const authorRef = this.auth.db.database.ref(`users/${articleData.author}`);
+      const authorRef = this.auth.db.database.ref(`staffProfiles/${this.article.author}`);
       authorRef.once('value', (snapshot) => {
-        let author = snapshot.val();
-        articleData.author = author;
-        this.auth.db.database.ref(`staffProfiles/${articleData.author.uid}`).once('value', (data) => {
-          let tempAuthor = data.val();
-          articleData.author['firstName'] = tempAuthor.firstName;
-          articleData.author['lastName'] = tempAuthor.lastName;
-        })
+        this.author = snapshot.val();
       }).then(() => {
+
         // Code to convert timestamp
-        const articleDate = new Date(articleData.writtenDate).toLocaleDateString('en-GB', {
+        const articleDate = new Date(this.article.writtenDate).toLocaleDateString('en-GB', {
           day: 'numeric',
           month: 'short',
           year: 'numeric',
         });
-        articleData.writtenDate = articleDate;
+        this.article.writtenDate = articleDate;
 
         // Set article data
-        this.article = articleData;
         this.dataLoaded = true;
         this.cd.detectChanges();
-      });
+      })
     });
   }
 
