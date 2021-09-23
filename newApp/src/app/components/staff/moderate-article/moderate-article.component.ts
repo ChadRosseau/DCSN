e2302a56437599ae9de692340207572f5670e796
Chad Rosseau <dahc.creeper@icloud.com>
2021-09-23 10:29:28 +0800

    moderation and onhold

--- newApp/src/app/components/staff/moderate-article/moderate-article.component.ts
+++ newApp/src/app/components/staff/moderate-article/moderate-article.component.ts
@@ -6,9 +6,10 @@ import { AuthService } from '@services/auth.service';
 import { SharedDataService } from '@services/shared-data.service';
 
 // Types
-import { Article } from '@interfaces/article';
+import { Article, ArticleInfo } from '@interfaces/article';
 import { Criteria, Moderation } from '@interfaces/moderation';
 import { StaffProfile } from '@interfaces/staff-profile';
+import { DatabaseReference } from '@angular/fire/database/interfaces';
 
 @Component({
   selector: 'app-moderate-article',
@@ -19,6 +20,7 @@ import { StaffProfile } from '@interfaces/staff-profile';
 export class ModerateArticleComponent implements OnInit {
   currentArticleId: string;
   article: Article | null = null;
+  articleInfo: ArticleInfo;
   author: StaffProfile | null;
   date: String = "";
   dataLoaded: boolean = false;
@@ -50,65 +52,86 @@ export class ModerateArticleComponent implements OnInit {
     this.currentArticleId = this.route.snapshot.paramMap.get('articleId');
 
     // Fetch data on current article from db.
-    const dbArticleRef = this.auth.db.database.ref(`articles/moderating/${this.currentArticleId}`);
+    const dbArticleRef: DatabaseReference = this.auth.db.database.ref(`articles/moderating/${this.currentArticleId}`);
     dbArticleRef.once('value', (snapshot) => {
-      let articleData = snapshot.val()
-      this.article = articleData;
-      console.log(this.article)
+      this.articleInfo = snapshot.val();
     }).then(() => {
-      // Get author data
-      const authorRef = this.auth.db.database.ref(`staffProfiles/${this.article.author}`);
-      authorRef.once('value', (snapshot) => {
-        this.author = snapshot.val();
-      }).then(() => {
-
-        // Convert timestamp
-        const articleDate = new Date(this.article.writtenDate).toLocaleDateString('en-GB', {
-          day: 'numeric',
-          month: 'short',
-          year: 'numeric',
-        });
-        this.date = articleDate;
-
-        // Get current moderation by user (if any)
-        if (this.article.moderations) {
-          if (this.article.moderations[this.auth.staffObject['uid']]) {
-            this.moderation = this.article.moderations[this.auth.staffObject['uid']];
-          }
+      // Get article body data from database.
+      const dbArticleBodyRef: DatabaseReference = this.auth.db.database.ref(`articleBodies/${this.currentArticleId}`);
+      dbArticleBodyRef.once('value', (snapshot) => {
+        // Combine article info with body for display.
+        this.article = {
+          ...this.articleInfo,
+          body: snapshot.val()
         }
+      }).then(() => {
+        // Get author data
+        const authorRef: DatabaseReference = this.auth.db.database.ref(`staffProfiles/${this.article.author}`);
+        authorRef.once('value', (snapshot) => {
+          this.author = snapshot.val();
+        }).then(() => {
 
-        // Set article data
-        this.dataLoaded = true;
-        this.cd.detectChanges();
+          // Convert timestamp
+          const articleDate = new Date(this.article.writtenDate).toLocaleDateString('en-GB', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric',
+          });
+          this.date = articleDate;
+
+          // Get current moderation by user (if any)
+          if (this.article.moderations) {
+            if (this.article.moderations[this.auth.staffObject['uid']]) {
+              this.moderation = this.article.moderations[this.auth.staffObject['uid']];
+            }
+          }
+
+          // Set article data
+          this.dataLoaded = true;
+          this.cd.detectChanges();
+        })
       })
-    });
+    })
   }
 
   submit() {
+    // Get timestamp for moderation.
     this.moderation.timestamp = new Date().getTime();
+    // If there are current other moderations, append the new moderation to the array.
     if (this.article.moderations != null) {
       this.article['moderations'][this.auth.staffObject['uid']] = this.moderation;
+
+      // Check to see if article has passed moderations.
+      let passCheck = true;
+
+      // If there are 2 or more moderations.
       if (Object.keys(this.article.moderations).length >= 2) {
-        let passCheck = true;
         Object.values(this.article.moderations).forEach((moderation: Moderation) => {
           if (moderation.publishReady == false) { passCheck = false }
         });
+        // If so, move to onhold.
         if (passCheck == true) {
           this.auth.db.database.ref(`articles/onhold/${this.currentArticleId}`).set(this.article);
           this.auth.db.database.ref(`articles/moderating/${this.currentArticleId}`).set(null);
+
+          // Otherwise, move back to drafts with feedback.
         } else {
           this.auth.db.database.ref(`articles/drafts/${this.currentArticleId}`).set(this.article);
           this.auth.db.database.ref(`articles/moderating/${this.currentArticleId}`).set(null);
         }
+        // If there are no existing moderations, create and set moderations property on article in db to this moderation.
       } else {
         this.auth.db.database.ref(`articles/moderating/${this.currentArticleId}/moderations/${this.auth.staffObject['uid']}`).set(this.moderation);
       }
     } else {
       this.auth.db.database.ref(`articles/moderating/${this.currentArticleId}/moderations/${this.auth.staffObject['uid']}`).set(this.moderation);
     }
+
+    // Redirect to home
     this.router.navigate(['/staff', 'overview']);
   }
 
+  // Format date
   makeDate(timestamp: number) {
     return new Date(timestamp.toString()).toLocaleDateString('en-GB', {
       day: 'numeric',
@@ -117,6 +140,7 @@ export class ModerateArticleComponent implements OnInit {
     });
   }
 
+  // Moving functions for auth.
   authMove(destination) {
     if (this.auth.staffObject.permission > 2) return;
     if (destination == 'live') {
--- newApp/src/app/components/staff/on-hold/on-hold.component.ts
+++ newApp/src/app/components/staff/on-hold/on-hold.component.ts
@@ -6,8 +6,9 @@ import { AuthService } from '@services/auth.service';
 import { SharedDataService } from '@services/shared-data.service';
 
 // Types
-import { Article } from '@interfaces/article';
+import { Article, ArticleInfo } from '@interfaces/article';
 import { StaffProfile } from '@interfaces/staff-profile';
+import { DatabaseReference } from '@angular/fire/database/interfaces';
 
 @Component({
   selector: 'app-on-hold',
@@ -17,6 +18,7 @@ import { StaffProfile } from '@interfaces/staff-profile';
 export class OnHoldComponent implements OnInit {
   currentArticleId: string;
   article: Article | null = null;
+  articleInfo: ArticleInfo;
   author: StaffProfile | null;
   date: String = "";
   dataLoaded: boolean = false;
@@ -35,31 +37,38 @@ export class OnHoldComponent implements OnInit {
     this.currentArticleId = this.route.snapshot.paramMap.get('articleId');
 
     // Fetch data on current article from db.
-    const dbArticleRef = this.auth.db.database.ref(`articles/onhold/${this.currentArticleId}`);
+    const dbArticleRef: DatabaseReference = this.auth.db.database.ref(`articles/onhold/${this.currentArticleId}`);
     dbArticleRef.once('value', (snapshot) => {
-      let articleData = snapshot.val()
-      this.article = articleData;
-      console.log(this.article)
-    }).then(() => {
-      // Get author data
-      const authorRef = this.auth.db.database.ref(`staffProfiles/${this.article.author}`);
-      authorRef.once('value', (snapshot) => {
-        this.author = snapshot.val();
+      this.articleInfo = snapshot.val();
+    }).then(() => {// Get article body data from database.
+      const dbArticleBodyRef: DatabaseReference = this.auth.db.database.ref(`articleBodies/${this.currentArticleId}`);
+      dbArticleBodyRef.once('value', (snapshot) => {
+        // Combine article info with body for display.
+        this.article = {
+          ...this.articleInfo,
+          body: snapshot.val()
+        }
       }).then(() => {
+        // Get author data
+        const authorRef: DatabaseReference = this.auth.db.database.ref(`staffProfiles/${this.article.author}`);
+        authorRef.once('value', (snapshot) => {
+          this.author = snapshot.val();
+        }).then(() => {
 
-        // Convert timestamp
-        const articleDate = new Date(this.article.writtenDate).toLocaleDateString('en-GB', {
-          day: 'numeric',
-          month: 'short',
-          year: 'numeric',
-        });
-        this.date = articleDate;
+          // Convert timestamp
+          const articleDate = new Date(this.article.writtenDate).toLocaleDateString('en-GB', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric',
+          });
+          this.date = articleDate;
 
-        // Set article data
-        this.dataLoaded = true;
-        this.cd.detectChanges();
-      })
-    });
+          // Set article data
+          this.dataLoaded = true;
+          this.cd.detectChanges();
+        })
+      });
+    })
   }
 
   submit(destination) {
